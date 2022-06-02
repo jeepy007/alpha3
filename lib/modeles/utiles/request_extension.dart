86e5d731f1eda91fde517b06e9ed6cc4cd7e7143
iMac-11 <klkjp007@gmail.comgit config --global user.name jeepy007git config user.email klkjp007@gmail.com>
2022-06-02 14:04:42 +0000

    dire

---
+++ lib/modeles/utiles/request_extension.dart
@@ -0,0 +1,41 @@
+import 'dart:async';
+import 'dart:convert';
+//import 'package:get_it/get_it.dart';
+import 'package:alpha3/modeles/User.dart';
+import 'package:http/http.dart' as http;
+import 'package:flutter/material.dart';
+
+class RequestExtension<T> {
+  static const String _urlEndpoint = 'https://restcountries.com/v3.1/name/';
+  static const String _urlEndpointSimple = 'https://restcountries.com/v3.1/name/';
+
+  
+  
+
+  Future<dynamic> get(String url) async {
+    print(url);
+    print("++++++++++++++++++++++++++++++++");
+    print(_urlEndpoint + url);
+
+    final response= await http.get(Uri.parse(_urlEndpoint + url),
+    headers:{"content-type":"application/Json;charset=utf-8"}
+
+   );
+   if (response.statusCode==200 || response.statusCode==201){
+     
+     print("**********************555555556666**************************");
+     switch (T){
+       case User:print((json.decode(response.body)[0])); 
+       print("0000000000000000000000000000");
+       return User.fromJson(json.decode(response.body)[0]) as T;
+       default: return User.fromJson(json.decode(response.body.toString())) as T;
+     };
+   };
+
+
+   
+  }
+
+
+
+}
