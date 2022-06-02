e5412df82730b5057618be417e1910b713f52388
iMac-11 <klkjp007@gmail.comgit config --global user.name jeepy007git config user.email klkjp007@gmail.com>
2022-06-02 14:59:31 +0000

    raz

--- lib/modeles/utiles/request_extension.dart
+++ lib/modeles/utiles/request_extension.dart
@@ -6,34 +6,50 @@ import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 
 class RequestExtension<T> {
-  static const String _urlEndpoint = 'https://restcountries.com/v3.1/name/';
-  static const String _urlEndpointSimple = 'https://restcountries.com/v3.1/name/';
+  static const String _urlEndpoint = 'http://15.188.11.154:3000/';
+  static const String _urlEndpointSimple = 'http://15.188.11.154:3000/';
 
   
   
 
-  Future<dynamic> get(String url) async {
+  Future<dynamic> loading(String url, dynamic data) async {
     print(url);
     print("++++++++++++++++++++++++++++++++");
     print(_urlEndpoint + url);
 
-    final response= await http.get(Uri.parse(_urlEndpoint + url),
-    headers:{"content-type":"application/Json;charset=utf-8"}
+    final response= await http.post(Uri.parse(_urlEndpoint + url),
+    headers:{"content-type":"application/Json;charset=utf-8"},
+    body: data
 
    );
    if (response.statusCode==200 || response.statusCode==201){
      
      print("**********************555555556666**************************");
+     debugPrint(response.body.toString());
+    // GetIt.l<AppService>().setPlayerId(jsonDecode(response.body)('data')('id').toString());
+     //GetIt.l<AppService>().setPlayerName(jsonDecode(response.body)('data')('prenom'));
+
+
+
      switch (T){
-       case User:print((json.decode(response.body)[0])); 
+       case User:print((json.decode(response.body))); 
        print("0000000000000000000000000000");
-       return User.fromJson(json.decode(response.body)[0]) as T;
+       return User.fromJson(json.decode(response.body)) as T;
        default: return User.fromJson(json.decode(response.body.toString())) as T;
      };
+   }
+   else {
+     String message ='';
+     if(jsonDecode(response.body)!=null){
+       message=jsonDecode(response.body);
+      }
+      else { 
+        message= response.reasonPhrase!;
+       }
+
+
+
    };
-
-
-   
   }
 
 
